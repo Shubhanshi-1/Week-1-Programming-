c816cf85b62882327da663f6cd041f2e3ebaff80
Shubhanshi <vermashashi3421@gmail.com>
2025-01-10 11:38:58 +0530

    Question 6

---
+++ Level_1_programming/DiscountedFee.java
@@ -0,0 +1,16 @@
+// Creating a class for calculating Discounted Fees 
+public class DiscountedFee {
+    public static void main(String[] args) {
+        // Fee and discount percentage
+        int fee = 125000;
+        int discountPercent = 10;
+
+        // Calculate discount amount and final fee
+        double discount = (discountPercent / 100.0) * fee;
+        double finalFee = fee - discount;
+
+        // Print the results
+        System.out.println("The discount amount is INR " + discount + 
+                           " and final discounted fee is INR " + finalFee);
+    }
+}
